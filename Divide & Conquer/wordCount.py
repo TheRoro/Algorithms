9e781ab13dec1843f9c92f2895260b38d06faeae
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-08-31 22:10:00 -0500

    changed to py

---
+++ Divide & Conquer/wordCount.py
@@ -0,0 +1,14 @@
+def wordCount(s, i, j, words):
+    if i == j:
+        if s[i] == " ":
+            return words+1
+        else:
+            return 0
+    else:
+        med = (i+j)//2
+        left = wordCount(s, i, med, words)
+        right = wordCount(s, med+1, j, words)
+        return left+right
+
+s = "How many words are here?"
+print(wordCount(s, 0, len(s)-1, 0) + 1)
