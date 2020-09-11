f126731e2c595f8d9584293fb573996ddc126987
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-09-10 21:34:57 -0500

    Quicksort

---
+++ Divide & Conquer/QuickSort.py
@@ -0,0 +1,26 @@
+#Quick Sort
+
+def partition(arr, low, high):
+    i = (low-1)
+    pivot = arr[high]
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i+=1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return(i+1)
+
+def quickSort(arr, low, high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
+        pi = partition(arr, low, high)
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+
+arr = [1,9,2,8,3,7,4,6,5]
+n = len(arr)
+quickSort(arr, 0, n-1)
+print(arr)
