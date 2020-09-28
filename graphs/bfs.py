d8837ba4bbf30ec2cb4950319c89404c7610226a
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-09-27 23:21:29 -0500

    Added bfs

---
+++ graphs/bfs.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+adj = [[1, 3],
+[0,2],
+[1,3],
+[0,1]]
+
+used = []
+s = 0
+q = []
+
+for i in range(len(adj)):
+    used.append(False)
+
+def bfs():
+    q.append(s)
+    used[s] = True
+    while(q):
+        v = q[0]
+        print("i'm in node:", v)
+        q.pop(0)
+        for u in adj[v]:
+            if(used[u] == False):
+                used[u] = True
+                q.append(u)
+
+bfs()
