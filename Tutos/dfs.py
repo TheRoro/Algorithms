f8b4608c4577b498d524b1e7b4bf46b8e7dc6ccc
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-09-30 12:05:09 -0500

    dfs tutos

--- Tutos/dfs.py
+++ Tutos/dfs.py
@@ -18,29 +18,3 @@ def dfs(v):
                 print(v, "es el antecesor de", u)
             dfs(u)
 
-#bfs
-
-adj = [[1, 3],
-[0,2],
-[1,3],
-[0,1]]
-
-used = []
-s = 0
-q = []
-
-for i in range(len(adj)):
-    used.append(False)
-
-def bfs():
-    q.append(s)
-    used[s] = True
-    while(q):
-        v = q.pop(0)
-        print("i'm in node:", v)
-        for u in adj[v]:
-            if(used[u] == False):
-                used[u] = True
-                q.append(u)
-
-bfs()
