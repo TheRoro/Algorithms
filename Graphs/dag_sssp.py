c5869be02d7053fac214103b0ebf505b2daef817
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-11-06 12:26:57 -0500

    Implemented DAG shortest path

--- Graphs/dag_sssp.py
+++ Graphs/dag_sssp.py
@@ -12,19 +12,49 @@ adj = [
 
 infinity = 10**10
 
-start = 0
-end = 4 # not necessary since DAG calculates a single source shortest path (SSSP)
+start = 3
+end = 6 # not necessary since DAG calculates a single source shortest path (SSSP)
 n = len(adj)
 dist = n * [infinity]
 pred = n * [None]
 path = []
+visited = [False]*n
+order = []
+
+def topoSort(G):
+    def dfsVisit(v):
+        if visited[v]: return
+        visited[v] = True
+        for u, _ in G[v]:
+            if not visited[u]:
+                dfsVisit(u)
+        order.append(v)
+
+    for v in range(n):
+        if not visited[v]:
+            dfsVisit(v)
+
+    return list(reversed(order))
 
 def dag_shortest():
-    topo_sort = topoSort(G)
+    dist[start] = 0
+    topo_sort = topoSort(adj)
+    for v in topo_sort:
+        for u, w in adj[v]:
+            if dist[v] + w < dist[u]:
+                dist[u] = dist[v] + w
+                pred[u] = v
 
-    for u in topo_sort:
-        for v, w in G[u]:
-            f = dist[u] + w
-            if dist[v] > f:
-                dist[v] = f
-                path[v] = u
+def restore_path():
+    v = end
+    while v != None:
+        path.append(v)
+        v = pred[v]
+    path.reverse()
+
+dag_shortest()
+restore_path()
+print(dist)
+print(pred)
+print(path)
+
