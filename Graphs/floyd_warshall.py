ee8247bc637722526047a04568fec841fc2c770f
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-11-11 21:08:16 -0500

    Modified Floyd-Warshall

--- Graphs/floyd_warshall.py
+++ Graphs/floyd_warshall.py
@@ -1,23 +1,23 @@
 def floydWarshall(G):
     n = len(G)
-    cost = [[10000000]*n for _ in range(n)]
-    path = [[-1]*n for _ in range(n)]
+    dist = [[10000000]*n for _ in range(n)]
+    pred = [[-1]*n for _ in range(n)]
 
     for u in range(n):
         for v, w in G[u]:
-            cost[u][v] = w
-            path[u][v] = u
+            dist[u][v] = w
+            pred[u][v] = u
 
     for k in range(n):
         for i in range(n):
             for j in range(n):
                 if i != j and k != j and k != i:
-                    f = cost[i][k] + cost[k][j]
-                    if f < cost[i][j]:
-                        cost[i][j] = f
-                        path[i][j] = path[k][j]
+                    f = dist[i][k] + dist[k][j]
+                    if f < dist[i][j]:
+                        dist[i][j] = f
+                        pred[i][j] = pred[k][j]
 
-    return path, cost
+    return pred, dist
 
 adj = [
 [],
@@ -29,8 +29,8 @@ adj = [
 [],
 [],
 ]
-path, dist = floydWarshall(adj)
-for el in path:
+pred, dist = floydWarshall(adj)
+for el in pred:
     print(el)
 for el in dist:
     print(el)
