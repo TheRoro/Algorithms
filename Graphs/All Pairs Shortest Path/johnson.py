7dc0155c5c7dd20b5608447840394dc5afe54f6e
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-11-19 14:27:43 -0500

    Finished Johnson's Algorithm implementation

--- Graphs/All Pairs Shortest Path/johnson.py
+++ Graphs/All Pairs Shortest Path/johnson.py
@@ -1,54 +1,85 @@
-#Bellman ford implementation
 adj = [
-[(1,10),(2,15)],
-[(0,10),(3,19)],
-[(0,15),(3,17),(4,13)],
-[(1,19),(2,17),(4,11)],
-[(2,13),(3,11)]
+[(1,-2)],
+[(2,-1)],
+[(0,4),(3,2),(4,-3)],
+[],
+[],
+[(3,1),(4,-4)]
 ]
 
-q = 5
+modified_adj = [
+[(1,-2)],
+[(2,-1)],
+[(0,4),(3,2),(4,-3)],
+[],
+[],
+[(3,1),(4,-4)]
+]
 
 infinity = 10**10
 negInfinity = -(infinity)
-
-adjPrima = [
-[(1,10),(2,15)],
-[(0,10),(3,19)],
-[(0,15),(3,17),(4,13)],
-[(1,19),(2,17),(4,11)],
-[(2,13),(3,11)],
-[(0,infinity),(1,infinity),(2,infinity),(3,infinity),(4,infinity)] #nuevo nodo Q
-]
-
-start = 0
 n = len(adj)
-dist = n * [infinity]
-pred = n * [None]
-path = []
-#Complexity of Bellman-Ford O(VE)
-def bellman_ford(s):
 
+def add_extra_node(modified_adj):
+    temp_adj = modified_adj
+    list_extra = []
+    for i in range(len(modified_adj)):
+        list_extra.append((i, 0))
+    temp_adj.append(list_extra)
+    return temp_adj
+
+def print_adj(adj):
+    for e in adj:
+        print(e)
+
+def bellman_ford(s, adjy):
+    adj_bellman = adjy
+    n = len(adjy)
+    dist = n * [infinity]
     dist[s] = 0
     for i in range(n-1):
         for v in range(n):
-            for u, w in adjPrima[v]:
+            for u, w in adj_bellman[v]:
                 if dist[v] + w < dist[u]:
                     dist[u] = dist[v] + w
-                    pred[u] = v
 
     for v in range(n):
-        for u, w in adjPrima[v]:
+        for u, w in adj_bellman[v]:
             if dist[v] + w < dist[u]:
                     dist[u] = negInfinity
-                    #pred[u] = None
-
-bellman_ford(start)
-print(dist)
+                    #There's a negative cycle
+    return dist
 
 
-def dijkstra(start):
+modified_adj = add_extra_node(modified_adj)
+temp_node = len(modified_adj)-1 #to define that temporal node is the last one in this implementation
 
+#print_adj(adj)
+#print_adj(modified_adj)
+
+temp_dist = bellman_ford(temp_node, modified_adj)
+
+h = temp_dist[0:temp_node]
+
+def replace_negative_edges(adj, h):
+    final_adj = []
+    for v in range(len(adj)):
+        final_adj.append([])
+        for u, w in adj[v]:
+            new_weight = w + h[v] - h[u]
+            tup = (u, new_weight)
+            final_adj[v].append(tup)
+    return final_adj
+
+final_adj = replace_negative_edges(adj, h)
+
+print_adj(final_adj)
+
+def dijkstra(s, adj):
+    dijkstra_adj = adj
+    start = s
+    dist = n * [infinity]
+    pred = n * [None]
     dist[start] = 0
 
     q = set()
@@ -66,8 +97,20 @@ def dijkstra(start):
                 dist[u] = dist[v] + weight
                 q.add((u, dist[u]))
                 pred[u] = v
+    return dist, pred
 
-for i in range(len(adj)):
-    dijkstra(i)
-    #print(dist)
-    print(pred)
+dist = []
+pred = []
+
+for i in range(n):
+    dist_i, pred_i = dijkstra(i, final_adj)
+    dist.append(dist_i)
+    pred.append(pred_i)
+
+for el in pred:
+    print(el)
+print()
+
+for el in dist:
+    print(el)
+print()
