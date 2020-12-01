ee178a66968c4b5291bf14f598259df96a9dfe10
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-12-01 12:35:39 -0500

    Added append push_back add emplace_back push at the end method for singly linked list :)

--- Data Structures/linked_list.py
+++ Data Structures/linked_list.py
@@ -6,6 +6,8 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tails = None
+        self.length = 0
 
     def printy(self):
         val = self.head
@@ -18,16 +20,40 @@ class LinkedList:
         while val.next != None:
             val = val.next
         val.next = node
+        self.length+=1
+    
+    def size(self):
+        return self.length
+
+    def appendy(self, node):
+        if(self.size() == 0):
+            self.head = node
+        elif(self.size() == 1):
+            self.tails = node
+            self.head.next = self.tails
+        else:
+            self.tails.next = node
+            self.tails = node
+        self.length+=1
 
 
 listy = LinkedList()
-listy.head = Node(1)
+#listy.head = Node(1)
 
-second = Node(2)
-third = Node(3)
-fourth = Node(4)
-listy.append(second)
-listy.append(third)
-listy.append(fourth)
+one = Node(1)
+two = Node(2)
+three = Node(3)
+four = Node(4)
+listy.appendy(one)
+listy.appendy(two)
+listy.appendy(three)
+listy.appendy(four)
+#second = Node(2)
+#third = Node(3)
+#fourth = Node(4)
+#listy.append(second)
+#listy.append(third)
+#listy.append(fourth)
 
 listy.printy()
+print(listy.size())
