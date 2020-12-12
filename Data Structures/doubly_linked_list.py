5e8de4cff2b157f5a8fa91543f18c557624d4108
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-12-12 09:53:10 -0500

    Added Insert method 📑
    Implemented the Insert At position method for a doubly linked list 📑->📑->📑

--- Data Structures/doubly_linked_list.py
+++ Data Structures/doubly_linked_list.py
@@ -44,15 +44,67 @@ class DoublyLinkedList:
             self.head = node
         self.length+=1
 
+    def insertAt(self, data, pos):
+        if type(pos) is not int:
+            return "Not a valid position"
+        elif pos < 0 or pos > self.length:
+            return False
+        
+        if pos == 0:
+            self.push(data)
+            #print("I'm a push")
+        elif pos == self.length:
+            self.append(data)
+            #print("I'm an append")
+        else:
+            node = Node(data)
+            #since I have 2 pointers I should start with the closest
+            diff1 = abs(0 - pos)
+            diff2 = abs(self.length - pos)
+
+            if diff1 > diff2:
+                #Position closer to the end (so I use the prev pointer)
+                temp1 = self.tails
+                temp2 = self.tails
+                i = self.length
+                while temp2.prev != None and i != pos:
+                    temp1 = temp2
+                    temp2 = temp2.prev
+                    i-=1
+                #print("I'm closer to the end")
+                temp1.prev = node
+                node.next = temp1
+                temp2.next = node
+                node.prev = temp2
+                self.length+=1
+            else:
+                #Position closer to the start or same length (so I use the next pointer)
+                temp1 = self.head
+                temp2 = self.head
+                i = 0
+                while temp2.next != None and i != pos:
+                    temp1 = temp2
+                    temp2 = temp2.next
+                    i+=1
+                #print("I'm closer to the start")
+                temp1.next = node
+                node.prev = temp1
+                node.next = temp2
+                temp2.prev = node
+                self.length+=1
+                
+
+
+
 listy = DoublyLinkedList()
 
 print(listy.length)
-listy.push("RORO")
-listy.push("CHINO")
+listy.insertAt("RORO", 0)
+listy.insertAt("CHINO", 1)
+listy.insertAt("JUANKA", 1)
+listy.insertAt("LEYVA", 2)
+print(listy.head.data)
 print(listy.head.next.data)
 print(listy.tails.prev.data)
-listy.push("JUANKA")
-listy.push("LEYVA")
 print(listy.tails.data)
-print(listy.tails.prev.data)
 print(listy.length)
