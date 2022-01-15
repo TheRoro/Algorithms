60b4cf64c66e3856296bfbfa81e344910d1e7c1e
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-01-15 13:32:42 -0500

    🧮 Fibonacci

---
+++ LeetCode/fibonacci.py
@@ -0,0 +1,33 @@
+def fib1(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    return fib1(n - 1) + fib1(n - 2)
+
+
+def fib2(n):
+    t1 = 0
+    t2 = 1
+    for i in range(n + 1):
+        t, t1, t2 = t1, t2, t1 + t2
+    return t
+
+
+def fib3(n):
+    dp = [-1]*(n+1)
+
+    if n < 1:
+        return 0
+
+    dp[0] = 0
+    dp[1] = 1
+
+    for i in range(2, len(dp)):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+
+
+print(fib3(0))
