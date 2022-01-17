552df04a6ead4fcdf9a65451af9dcbda16176cbc
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-01-17 07:46:40 -0500

    🧮 Find Complement

---
+++ LeetCode/find_complement.py
@@ -0,0 +1,13 @@
+def findComplement(num: int) -> int:
+    binstr = bin(num)[2:]
+    ans = []
+    for c in binstr:
+        if c == '0':
+            ans.append('1')
+        else:
+            ans.append('0')
+
+    return int(''.join(ans), 2)
+
+
+print(findComplement(5))
