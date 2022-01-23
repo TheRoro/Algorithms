7ed0769c2fa49b4890ea097b85f402f16dada664
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-01-23 13:38:46 -0500

    🧮 Uncommon Words

---
+++ LeetCode/uncommon_words_from_two_sentences.py
@@ -0,0 +1,28 @@
+def uncommonFromSentences(s1: str, s2: str):
+    list1 = s1.split(" ")  # ["this", "apple", "is", "sweet"]
+    list2 = s2.split(" ")  # ["this", "apple", "is", "sour"]
+
+    memo = {}
+
+    for word in list1:
+        if word in memo:
+            memo[word] += 1
+        else:
+            memo[word] = 1
+
+    for word in list2:
+        if word in memo:
+            memo[word] += 1
+        else:
+            memo[word] = 1
+
+    ans = []
+
+    for word in memo:
+        if memo[word] == 1:
+            ans.append(word)
+
+    return ans
+
+
+print(uncommonFromSentences("this apple is sweet", "this apple is sour"))
