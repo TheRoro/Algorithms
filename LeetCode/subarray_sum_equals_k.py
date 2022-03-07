7189af348f04ce02b7d446dac1bce0048ade6fa0
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-03-07 10:37:01 -0500

    🧮 Subarray Sum Equals K

---
+++ LeetCode/subarray_sum_equals_k.py
@@ -0,0 +1,15 @@
+def sub_array_sum(nums, k):
+    hashmap = {0: 1}
+    current_sum = 0
+    n_sub_arrays = 0
+
+    for n in nums:
+        current_sum += n
+        diff = current_sum - k
+        n_sub_arrays += hashmap.get(diff, 0)
+        hashmap[current_sum] = hashmap.get(current_sum, 0) + 1
+
+    return n_sub_arrays
+
+
+print(sub_array_sum([1, 1, 1], 2))
