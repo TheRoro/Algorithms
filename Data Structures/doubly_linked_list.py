fcaff0f7115cb75de792d2bed47e02a92d084f7a
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-03-21 21:20:20 -0500

    🔨 Doubly Linked List

--- Data Structures/doubly_linked_list.py
+++ Data Structures/doubly_linked_list.py
@@ -4,6 +4,7 @@ class Node:
         self.prev = None
         self.next = None
 
+
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
@@ -16,16 +17,16 @@ class DoublyLinkedList:
     def print(self):
         current = self.head
 
-        while current != None:
+        while current is not None:
             print(current.data)
             current = current.next
 
     def append(self, data):
         node = Node(data)
 
-        if(self.size() == 0):
+        if self.size() == 0:
             self.head = node
-        elif(self.size() == 1):
+        elif self.size() == 1:
             self.tails = node
             self.head.next = self.tails
             self.tails.prev = self.head
@@ -33,14 +34,14 @@ class DoublyLinkedList:
             self.tails.next = node
             node.prev = self.tails
             self.tails = node
-        self.length+=1
-    
+        self.length += 1
+
     def push(self, data):
         node = Node(data)
 
-        if(self.size() == 0):
+        if self.size() == 0:
             self.head = node
-        elif(self.size() == 1):
+        elif self.size() == 1:
             self.tails = self.head
             self.head = node
             self.head.next = self.tails
@@ -49,57 +50,57 @@ class DoublyLinkedList:
             self.head.prev = node
             node.next = self.head
             self.head = node
-        self.length+=1
+        self.length += 1
 
-    def insertAt(self, data, pos):
+    def insert_at(self, data, pos):
         if type(pos) is not int:
             return "Not a valid position"
         elif pos < 0 or pos > self.length:
             return False
-        
+
         if pos == 0:
             self.push(data)
-            #print("I'm a push")
+            # print("I'm a push")
         elif pos == self.length:
             self.append(data)
-            #print("I'm an append")
+            # print("I'm an append")
         else:
             node = Node(data)
-            #since I have 2 pointers I should start with the closest
+            # since I have 2 pointers I should start with the closest
             diff1 = abs(0 - pos)
             diff2 = abs(self.length - pos)
 
             if diff1 > diff2:
-                #Position closer to the end (so I use the prev pointer)
+                # Position closer to the end (so I use the prev pointer)
                 temp1 = self.tails
                 temp2 = self.tails
                 i = self.length
-                while temp2.prev != None and i != pos:
+                while temp2.prev is not None and i != pos:
                     temp1 = temp2
                     temp2 = temp2.prev
-                    i-=1
-                #print("I'm closer to the end")
+                    i -= 1
+                # print("I'm closer to the end")
                 temp1.prev = node
                 node.next = temp1
                 temp2.next = node
                 node.prev = temp2
-                self.length+=1
+                self.length += 1
             else:
-                #Position closer to the start or same length (so I use the next pointer)
+                # Position closer to the start or same length (so I use the next pointer)
                 temp1 = self.head
                 temp2 = self.head
                 i = 0
-                while temp2.next != None and i != pos:
+                while temp2.next is not None and i != pos:
                     temp1 = temp2
                     temp2 = temp2.next
-                    i+=1
-                #print("I'm closer to the start")
+                    i += 1
+                # print("I'm closer to the start")
                 temp1.next = node
                 node.prev = temp1
                 node.next = temp2
                 temp2.prev = node
-                self.length+=1
-    
+                self.length += 1
+
     def reverse(self):
         first = None
         second = self.head
@@ -107,30 +108,28 @@ class DoublyLinkedList:
 
         self.tails = second
 
-        while second != None:
+        while second is not None:
             second.next = first
             second.prev = third
             first = second
             second = third
-            if third != None:
+            if third is not None:
                 third = third.next
-        
+
         self.head = first
-                
 
 
+_list = DoublyLinkedList()
 
-listy = DoublyLinkedList()
+print(_list.length)
+_list.insert_at("John", 0)
+_list.insert_at("Mark", 1)
+_list.insert_at("Bob", 1)
+_list.insert_at("Steve", 2)
 
-print(listy.length)
-listy.insertAt("RORO", 0)
-listy.insertAt("CHINO", 1)
-listy.insertAt("JUANKA", 1)
-listy.insertAt("LEYVA", 2)
-
-listy.print()
-listy.reverse()
+_list.print()
+_list.reverse()
 print("Reversed doubly linked list")
-listy.print()
+_list.print()
 
-print(listy.length)
+print(_list.length)
