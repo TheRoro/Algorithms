be98ba47c298eb1d3519effc3ea605fca0015344
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-04-21 11:14:03 -0500

    🧮 Find Minimum In Rotated Array

---
+++ LeetCode/find_minimum_in_rotated_array.py
@@ -0,0 +1,18 @@
+def find_min(nums) -> int:
+    res = nums[0]
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        if nums[l] <= nums[r]:
+            res = min(res, nums[l])
+            break
+
+        mid = (l + r) // 2
+        res = min(res, nums[mid])
+
+        if nums[mid] >= nums[l]:
+            l = mid + 1
+        else:
+            r = mid - 1
+
+    return res
