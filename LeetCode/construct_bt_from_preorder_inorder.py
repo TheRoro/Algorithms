159f6221866dd1d34899a9f885f3aa09f8e1b16b
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-05-19 12:15:12 -0500

    🧮 Construct Binary Tree

---
+++ LeetCode/construct_bt_from_preorder_inorder.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def build_tree(preorder, inorder):
+    if not preorder or not inorder:
+        return None
+
+    root = TreeNode(preorder[0])
+    mid = inorder.index(preorder[0])
+
+    root.left = build_tree(preorder[1:mid + 1], inorder[:mid])
+    root.right = build_tree(preorder[mid + 1:], inorder[mid + 1:])
+
+    return root
