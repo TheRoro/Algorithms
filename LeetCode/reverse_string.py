b8a6013bf17c8d00809445fc19d4d90aa8a1ceb1
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-06-13 10:10:00 -0500

    🔨 Reverse String

--- LeetCode/reverse_string.py
+++ LeetCode/reverse_string.py
@@ -1,7 +1,6 @@
 def reverse_string(s) -> None:
     for i in range(len(s) // 2):
         s[i], s[len(s) - 1 - i] = s[len(s) - 1 - i], s[i]
-    print(s)
 
 
 reverse_string(["h", "e", "l", "l", "o"])
